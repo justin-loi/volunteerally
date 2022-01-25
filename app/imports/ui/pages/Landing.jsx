4efff1489d680918fae3c12a3ffb2e5a7d188292
nicholasbcarr <carrn7@hawaii.edu>
2022-01-24 14:18:04 -1000

    Landing page work

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -4,16 +4,22 @@ import { PAGE_IDS } from '../utilities/PageIDs';
 
 /** A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <Grid id={PAGE_IDS.LANDING} verticalAlign='middle' textAlign='center' container>
-
-    <Grid.Column width={4}>
-      <Image size='small' circular src="/images/meteor-logo.png" />
-    </Grid.Column>
-
-    <Grid.Column width={8}>
-      <h1>Welcome to this template</h1>
-      <p>Now get to work and modify this app!</p>
-    </Grid.Column>
+  <Grid id={PAGE_IDS.LANDING} verticalAlign='middle' textAlign='center' rows='equal' className="landing-background-top" container>
+    <Grid.Row>
+      <Image className="landing-volunteer-ally-logo" size='medium' circular src="/images/volunteer-ally-temp-logo.png" centered/>
+    </Grid.Row>
+    <Grid.Row>
+      <p size="huge" className="landing-header-1">
+        A Better Way to Volunteer!
+      </p>
+    </Grid.Row>
+    <Grid.Row>
+      <p className="landing-header-2">
+        We connect passionate volunteers with charitable organizations in order to build community.<br/> Let us help you easily find service opportunities for organizations in your area of interest.
+      </p>
+    </Grid.Row>
+  </Grid>
+  <Grid>
 
   </Grid>
 );
