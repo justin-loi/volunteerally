d8679112843d50369cc995afbe9aa34fcc822796
nicholasbcarr <carrn7@hawaii.edu>
2022-01-25 18:10:27 -1000

    mockup ready for review

--- app/imports/ui/components/LandingEventCalendarSection.jsx
+++ app/imports/ui/components/LandingEventCalendarSection.jsx
@@ -3,8 +3,13 @@ import { Grid, Image } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const LandingEventCalendarSection = () => (
-  <Grid divided='horizontally' verticalAlign='middle' textAlign='center' rows='equal' container>
-    <Grid.Row columns='equal'>
+  <Grid verticalAlign='middle' textAlign='center' rows='equal'>
+    <Grid.Row>
+      <p className="landing-header-3">
+        Check Out Our Latest Opportunities
+      </p>
+    </Grid.Row>
+    <Grid.Row>
       <Image src="/images/events-calendar-cards-mockup.png"/>
     </Grid.Row>
   </Grid>
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import LandingTopSection from '../components/LandingTopSection';
 import LandingMiddleSection from '../components/LandingMiddleSection';
 import LandingEventCalendarSection from '../components/LandingEventCalendarSection';
+import LandingOpportunitiesByCategorySection from '../components/LandingOpportunitiesByCategorySection';
+import LandingBottomSection from '../components/LandingBottomSection';
 
 /** A simple static component to render some text for the landing page. */
 const Landing = () => (
@@ -9,6 +11,8 @@ const Landing = () => (
     <LandingTopSection/>
     <LandingMiddleSection/>
     <LandingEventCalendarSection/>
+    <LandingOpportunitiesByCategorySection/>
+    <LandingBottomSection/>
   </div>
 );
 
