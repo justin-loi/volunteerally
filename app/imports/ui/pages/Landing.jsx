2ab9ae6185d5ae405c82c856fff414e04a9cc70d
nicholasbcarr <carrn7@hawaii.edu>
2022-02-02 14:14:24 -1000

    fixed typos, bugs and put landing files in a directory

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import LandingTopSection from '../components/LandingTopSection';
-import LandingMiddleSection from '../components/LandingMiddleSection';
-import LandingEventCalendarSection from '../components/LandingEventCalendarSection';
-import LandingOpportunitiesByCategorySection from '../components/LandingOpportunitiesByCategorySection';
-import LandingBottomSection from '../components/LandingBottomSection';
+import LandingTopSection from '../components/landing/LandingTopSection';
+import LandingMiddleSection from '../components/landing/LandingMiddleSection';
+import LandingEventCalendarSection from '../components/landing/LandingEventCalendarSection';
+import LandingOpportunitiesByCategorySection from '../components/landing/LandingOpportunitiesByCategorySection';
+import LandingBottomSection from '../components/landing/LandingBottomSection';
 
 /** A simple static component to render some text for the landing page. */
 const Landing = () => (
