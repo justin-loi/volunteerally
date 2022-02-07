af5398013c0ffb079e01f7ed217939cf55c5269d
sulao1999 <59861460+sulao1999@users.noreply.github.com>
2022-02-06 14:36:14 -1000

    Update VolunteerSignup.jsx

--- app/imports/ui/pages/VolunteerSignup.jsx
+++ app/imports/ui/pages/VolunteerSignup.jsx
@@ -13,6 +13,12 @@ import { signUpNewVolunteerMethod } from '../../api/volunteer/VolunteerProfileCo
 const genderAllowValues = ['Male', 'Female', 'Other', 'Prefer Not to Say'];
 const genderComponentID = [COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_GENDER_MALE, COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_GENDER_FEMALE,
   COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_GENDER_OTHER, COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_GENDER_NO_SAY];
+const interestsAllowValues = ['Child/Family Support', 'COVID-19 Recovery', 'Crisis/Disaster Relief', 'Education', 'Environment',
+  'Elderly/Senior Care', 'Food Banks', 'Housing', 'Homelessness/Poverty', 'Special Needs'];
+const specialSkillsAllowValues = ['Construction', 'Education', 'Engineering', 'Event Planning', 'Sales/Marketing', 'Technology',
+  'Graphic/Web Design', 'CPR (Certification Required)', 'First Aid (Certification Required)', 'Nursing (CNA/RNA Certified)', 'Other'];
+const environmentalPreferenceAllowValues = ['Outdoor', 'Both', 'No Preference'];
+const availabilityAllowValues = ['Once a week', '1-3 times a week', 'More than 3 times a week', 'Weekends only', 'Weekdays onlymetoer'];
 
 // username, email, password, timeTracker, dob, firstName, lastName, gender,
 // address, city, state, zipCode_postalCode, phoneNumber,
@@ -47,13 +53,144 @@ const bridge = new SimpleSchema2Bridge(formSchema);
  */
 const Signup = ({ location }) => {
   const [redirectToReferer, setRedirectToReferer] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState('');
   const [gender, setGender] = useState('');
-  const [signUpError, setSignUpError] = useState(false);
+  const [interests, setInterests] = useState([]);
+  const [specialSkills, setSpecialSkills] = useState([]);
+  const [environmentalPreference, setEnvironmentalPreference] = useState('');
+  const [availability, setAvailability] = useState([]);
+  const [privacyPolicy, setPrivacyPolicy] = useState('');
+  // Array(arraySize).fill(value)
+  const [isValueEmpty, setIsValueEmpty] = useState(Array(2).fill(true));
+
+  const checkPassword = (p1, p2) => {
+    if (p1 === p2) {
+      return true;
+    }
+    swal('Error!', 'Your passwords do not match', 'error');
+    return false;
+  };
+
+  // reference: https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript
+  const validateEmail = (email) => String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    );
+
+  const checkEmail = (email) => {
+    if (validateEmail(email)) {
+      return true;
+    }
+    swal('Error!', 'Please enter a valid email ', 'error');
+    return false;
+  };
+
+  const isNotEmpty = (value) => (!value);
+
+  const agreePolicyAndTerm = (value) => {
+    if (!value) {
+      swal('Error!', 'Please confirm that you agree to our Privacy Policy and Term & Conditions', 'error');
+      return false;
+    }
+    return true;
+  };
+
+  const setIsValueEmptyHelper = (index, value) => {
+    isValueEmpty[index] = value;
+    setIsValueEmpty(isValueEmpty);
+  };
+
+  const checkboxHelper = (array, value) => {
+    // Reference: https://stackoverflow.com/questions/5767325/how-can-i-remove-a-specific-item-from-an-array
+    const index = array.indexOf(value);
+    if (index > -1) {
+      array.splice(index, 1);
+    } else {
+      array.push(value);
+    }
+    return array;
+  };
+
+  // reference: https://stackoverflow.com/questions/6177975/how-to-validate-date-with-format-mm-dd-yyyy-in-javascript
+  // Validates that the input string is a valid date formatted as "mm/dd/yyyy"
+  const isValidDate = (dateString) => {
+    // declare and initialize variables
+    const today = new Date();
+    // First check for the pattern
+    if (!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString)) {
+      setIsValueEmptyHelper(0, true);
+      swal('Error!', 'Please enter the correct date format mm/dd/yyyy', 'error');
+      return false;
+    }
+    // Parse the date parts to integers
+    const parts = dateString.split('/');
+    const day = parseInt(parts[1], 10);
+    const month = parseInt(parts[0], 10);
+    const year = parseInt(parts[2], 10);
+    // Check the ranges of month and year
+    if (year < (today.getFullYear() - 300) || year > today.getFullYear() || month === 0 || month > 12) {
+      setIsValueEmptyHelper(0, true);
+      swal('Error!', 'Invalid date', 'error');
+      return false;
+    }
+    const monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    // Adjust for leap years
+    if (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)) {
+      monthLength[1] = 29;
+    }
+    // Check the range of the day
+    if (day < 0 && day >= monthLength[month - 1]) {
+      setIsValueEmptyHelper(0, true);
+      swal('Error!', 'Invalid date', 'error');
+      return false;
+    }
+
+    // Checks are volunteers over 16
+    if ((today.getFullYear() - year) <= 16) {
+      if ((today.getFullYear() - year) === 16 && today.getMonth() >= month) {
+        return true;
+      }
+      setIsValueEmptyHelper(0, true);
+      swal('Sorry', 'You must be at least 16 years old to join Volunteer Ally', 'warning');
+      return false;
+    }
+    return true;
+  };
 
   const handleChange = (e, { name, value }) => {
     switch (name) {
+    case 'dateOfBirth':
+      setIsValueEmptyHelper(0, isNotEmpty(value));
+      setDateOfBirth(value);
+      break;
+    case 'confirmPassword':
+      setConfirmPassword(value);
+      break;
     case 'gender':
       setGender(value);
+      break;
+    case 'interests':
+      setInterests(checkboxHelper(interests, value));
+      break;
+    case 'specialSkills':
+      setSpecialSkills(checkboxHelper(specialSkills, value));
+      break;
+    case 'environmentalPreference':
+      setEnvironmentalPreference(value);
+      break;
+    case 'availability':
+      setAvailability(checkboxHelper(availability, value));
+      break;
+    case 'privacyPolicy':
+      if (privacyPolicy === '') {
+        setPrivacyPolicy(value);
+        setIsValueEmptyHelper(1, false);
+      } else {
+        setPrivacyPolicy('');
+        setIsValueEmptyHelper(1, true);
+      }
       break;
     default:
         // do nothing.
@@ -62,28 +199,28 @@ const Signup = ({ location }) => {
 
   /* Handle Signup submission. Create user account and a profile entry, then redirect to the home page. */
   const submit = (data, formRef) => {
-    console.log(data);
-    signUpNewVolunteerMethod.callPromise(data)
-      .catch(error => {
-        setSignUpError(true);
-        swal('Error', error.message, 'error');
-        console.error(error);
-      })
-      .then(() => {
-        // Not sure why it catches the error but still executes
-        if (!signUpError) {
+    if (isValidDate(data.dob) && checkPassword(data.password, confirmPassword) && agreePolicyAndTerm(privacyPolicy) && checkEmail(data.email)) {
+      signUpNewVolunteerMethod.callPromise(data)
+        .catch(error => {
+          swal('Error', error.message, 'error');
+        })
+        .then(() => {
+          // Not sure why it catches the error but still executes
           formRef.reset();
           setGender('');
+          setInterests([]);
+          setSpecialSkills([]);
+          setEnvironmentalPreference('');
+          setAvailability([]);
           swal({
             title: 'Signed Up',
             text: 'You now have an account. Next you need to login.',
             icon: 'success',
             timer: 1500,
           });
-          setRedirectToReferer(false);
-        }
-        setSignUpError(false); // reset no matter what
-      });
+          setRedirectToReferer(true);
+        });
+    }
   };
 
   /* Display the signup form. Redirect to add page after successful registration and login. */
@@ -94,22 +231,41 @@ const Signup = ({ location }) => {
   }
   let fRef = null;
   return (
-    <Container id={PAGE_IDS.SIGN_UP}>
+    <Container id={PAGE_IDS.VOLUNTEER_SIGNUP}>
       <Grid textAlign="center" verticalAlign="middle" centered columns={2}>
         <Grid.Column>
           <Header as="h2" textAlign="center">
-              Register your account
+            Volunteer Sign Up Form
           </Header>
 
           <AutoForm ref={ref => {
             fRef = ref;
           }} schema={bridge} onSubmit={data => submit(data, fRef)}>
             <Segment>
-              <TextField name='username'id={COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_USERNAME}/>
+              <TextField name='username' id={COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_USERNAME}/>
               <TextField name='email' type='email' label='E-mail' id={COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_EMAIL}/>
               <TextField name='password' type='password' id={COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_PASSWORD}/>
+              <Form.Input
+                label="Confirm Password"
+                id={COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_CONFIRM_PASSWORD}
+                name="confirmPassword"
+                type="password"
+                placeholder="Confirm Your Password"
+                required
+                onChange={handleChange}
+              />
               <HiddenField name='timeTracker' value='0'/>
-              <TextField name='dob' label='Date of Birth' id={COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_BIRTH}/>
+              <HiddenField name='dob' label='Date of Birth (You must be at least 16 years old to join Volunteer Ally)' value={dateOfBirth} />
+              <Form.Input
+                label="Date Of Birth (You must be at least 16 years old to join Volunteer Ally)"
+                id={COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_BIRTH}
+                icon="calendar alternate outline"
+                iconPosition="left"
+                name="dateOfBirth"
+                placeholder="mm/dd/yyyy"
+                onChange={handleChange}
+                error={ isValueEmpty[0] }
+              />
               <div className="two fields">
                 <div className="field">
                   <TextField name='firstName' label='First Name' id={COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_FIRST}/>
@@ -123,7 +279,7 @@ const Signup = ({ location }) => {
                 <label>Gender: </label>
                 {genderAllowValues.map((value, index) => (
                   <Form.Radio
-                    key={index}
+                    key={`volunteer-signup-gender-${index}`}
                     id={genderComponentID[index]}
                     label={value}
                     name='gender'
@@ -152,16 +308,141 @@ const Signup = ({ location }) => {
                     id={COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_PHONE}/>
                 </div>
               </div>
-              <TextField name='interests'/>
-              <TextField name='specialSkills' label='Special Skills'/>
-              <TextField name='environmentalPreference' label='Environmental Preference'/>
-              <TextField name='availability'/>
+              <HiddenField name='interests' value={interests}/>
+              <label style={{ paddingTop: '20px' }}>Interests: </label>
+              <Form.Group>
+                <Grid columns={2}>
+                  <Grid.Row style={{ paddingLeft: '8px' }}>
+                    <Grid.Column>
+                      <Form.Checkbox
+                        id={COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_ANIMALS}
+                        label='Animal Welfare/Rescue'
+                        name='interests'
+                        value='Animal Welfare/Rescue'
+                        onChange={handleChange}
+                      />
+                    </Grid.Column>
+                    {interestsAllowValues.map((value, index) => (
+                      <Grid.Column key={`volunteer-signup-grid-interests-${value}`}>
+                        <Form.Checkbox
+                          key={`volunteer-signup-interests-${index}`}
+                          id={`volunteer-signup-interests-${value}`}
+                          label={value}
+                          name='interests'
+                          value={value}
+                          onChange={handleChange}
+                        />
+                      </Grid.Column>
+                    ))}
+                  </Grid.Row>
+                </Grid>
+              </Form.Group>
+              <HiddenField name='specialSkills' label='Special Skills (optional)' value={specialSkills}/>
+              <label style={{ paddingTop: '20px' }}>Special Skills (optional) </label>
+              <Form.Group>
+                <Grid columns={2}>
+                  <Grid.Row style={{ paddingLeft: '8px' }}>
+                    <Grid.Column>
+                      <Form.Checkbox
+                        id={COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_AGRICULTURE}
+                        label='Agriculture'
+                        name='specialSkills'
+                        value='Agriculture'
+                        onChange={handleChange}
+                      />
+                    </Grid.Column>
+                    {specialSkillsAllowValues.map((value, index) => (
+                      <Grid.Column key={`volunteer-signup-grid-skills-${value}`}>
+                        <Form.Checkbox
+                          key={`volunteer-signup-skill-${index}`}
+                          id={`volunteer-signup-skill-${value}`}
+                          label={value}
+                          name='specialSkills'
+                          value={value}
+                          onChange={handleChange}
+                        />
+                      </Grid.Column>
+                    ))}
+                  </Grid.Row>
+                </Grid>
+              </Form.Group>
+              <HiddenField name='environmentalPreference' label='Environmental Preference' value={environmentalPreference}/>
+              <label>Environmental Preference: </label>
+              <Form.Group inline>
+                <Form.Radio
+                  id={COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_INDOOR}
+                  label='Indoor'
+                  name='environmentalPreference'
+                  value='Indoor'
+                  checked={environmentalPreference === 'Indoor'}
+                  onChange={handleChange}
+                />
+                {environmentalPreferenceAllowValues.map((value, index) => (
+                  <Form.Radio
+                    key={`volunteer-signup-environmental-preference-${index}`}
+                    id={`volunteer-signup-environmental-preference-${index}`}
+                    label={value}
+                    name='environmentalPreference'
+                    value={value}
+                    checked={environmentalPreference === value}
+                    onChange={handleChange}
+                  />
+                ))}
+              </Form.Group>
+              <HiddenField name='availability' value={availability}/>
+              <label style={{ paddingTop: '20px' }}>Availability </label>
+              <Form.Group>
+                <Grid columns={2}>
+                  <Grid.Row style={{ paddingLeft: '8px' }}>
+                    <Grid.Column>
+                      <Form.Checkbox
+                        label='One-Time'
+                        name='availability'
+                        value='One-Time'
+                        onChange={handleChange}
+                      />
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Form.Checkbox
+                        id={COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_MONTHLY}
+                        label='Once a month'
+                        name='availability'
+                        value='Once a month'
+                        onChange={handleChange}
+                      />
+                    </Grid.Column>
+                    {availabilityAllowValues.map((value, index) => (
+                      <Grid.Column key={`volunteer-signup-grid-availability-${value}`}>
+                        <Form.Checkbox
+                          key={`volunteer-signup-availability-${index}`}
+                          id={`volunteer-signup-availability-${value}`}
+                          label={value}
+                          name='availability'
+                          value={value}
+                          onChange={handleChange}
+                        />
+                      </Grid.Column>
+                    ))}
+                  </Grid.Row>
+                </Grid>
+              </Form.Group>
+              <Link to="/privacy" target="_blank" >Privacy Policy</Link>
+              <br/>
+              <Link to="/terms" target="_blank" >Term & Conditions</Link>
+              <Form.Checkbox
+                id={COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_POLICY}
+                label='Please confirm that you agree to our Privacy Policy and Term & Conditions'
+                name = 'privacyPolicy'
+                value = 'agree'
+                onChange={handleChange}
+                error={ isValueEmpty[1] }
+              />
               <SubmitField value='Sign up' id={COMPONENT_IDS.VOLUNTEER_SIGNUP_FORM_SUBMIT}/>
               <ErrorsField />
             </Segment>
           </AutoForm>
           <Message>
-              Already have an account? Login <Link to="/signin">here</Link>
+              Already have an volunteer? Login <Link to="/signin">here</Link>
           </Message>
         </Grid.Column>
       </Grid>
