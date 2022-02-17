209e5b686e275407b25e88e6eb96e83c6ae7563d
glee25 <59935750+glee25@users.noreply.github.com>
2022-02-17 01:40:21 -1000

    added image button, made upgrades

--- app/imports/ui/pages/AddEvent.jsx
+++ app/imports/ui/pages/AddEvent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Grid, Segment, Header } from 'semantic-ui-react';
-import { AutoForm, ErrorsField, SubmitField, TextField, LongTextField } from 'uniforms-semantic';
+import { Grid, Segment, Header, Button } from 'semantic-ui-react';
+import { AutoForm, ErrorsField, SubmitField, TextField, LongTextField, RadioField, HiddenField } from 'uniforms-semantic';
 import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
@@ -11,10 +11,10 @@ import { PAGE_IDS } from '../utilities/PageIDs';
 // Create a schema to specify the structure of the data to appear in the form.
 const formSchema = new SimpleSchema({
   eventName: String,
-  address: String,
+  location: String,
   category: String,
   description: String,
-
+  eventImage: String
 });
 
 const bridge = new SimpleSchema2Bridge(formSchema);
@@ -23,9 +23,9 @@ const bridge = new SimpleSchema2Bridge(formSchema);
 class AddEvent extends React.Component {
 
   submit(data, formRef) {
-    const { eventName, address, category, description } = data;
+    const { eventName, address, category, description, image } = data;
     const owner = Meteor.user().username;
-    Events.collection.insert({ eventName, address, category, description, owner },
+    Events.collection.insert({ eventName, location, category, description, eventImage, owner },
       (error) => {
         if (error) {
           swal('Error', error.message, 'error');
@@ -45,9 +45,14 @@ class AddEvent extends React.Component {
           <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)} >
             <Segment>
               <TextField name='eventName'/>
-              <TextField name='address'/>
-              <TextField name='category'/>
+              <TextField name='location'/>
+              <RadioField name='category'/>
+              <a className="ui label">Online<i className="delete icon"></i></a>
+              <a className="ui label">In-Person<i className="delete icon"></i></a>
+              <a className="ui label">etc.<i className="delete icon"></i></a>
               <LongTextField name='description'/>
+              <RadioField name='eventImage'/>
+              <Button class='ui labeled icon button'><i className="mail icon"></i>Choose Image</Button>
               <SubmitField value='Submit'/>
               <ErrorsField/>
             </Segment>
