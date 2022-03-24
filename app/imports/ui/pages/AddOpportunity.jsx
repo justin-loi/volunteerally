1022d4c905aecf4a1b8973f4c02979d090a72ccc
Gerald Lee <59935750+glee25@users.noreply.github.com>
2022-03-24 00:12:30 -1000

    Added to AddOppurtunity

--- app/imports/ui/pages/AddOpportunity.jsx
+++ app/imports/ui/pages/AddOpportunity.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Segment, Header, Button } from 'semantic-ui-react';
+import { Grid, Segment, Header } from 'semantic-ui-react';
 import { AutoForm, ErrorsField, SubmitField, TextField, LongTextField, RadioField } from 'uniforms-semantic';
 import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
@@ -51,11 +51,25 @@ class AddOpportunity extends React.Component {
               <TextField name='date'/>
               <TextField name='time'/>
               <RadioField name='categories'/>
-              <a className="ui label">Online<i className="delete icon"/></a>
-              <a className="ui label">In-Person<i className="delete icon"/></a>
+              <div className="ui form">
+                <div className="field">
+                  <select multiple="" className="ui dropdown">
+                    <option value="">Select Option</option>
+                    <option value="Online">Online</option>
+                    <option value="In-Person">In-Person</option>
+                    <option value="Hybrid">Hybrid</option>
+                  </select>
+                </div>
+              </div>
               <LongTextField name='description'/>
               <RadioField name='eventImage'/>
-              <Button class='ui labeled icon button'><i className="upload icon"/>Upload Image</Button>
+              <div className="ui placeholder segment">
+                <div className="ui icon header">
+                  <i className="pdf file outline icon"> </i>
+                  No image uploaded.
+                </div>
+                <div className="ui primary button">Add Image</div>
+              </div>
               <SubmitField value='Submit'/>
               <ErrorsField/>
             </Segment>
