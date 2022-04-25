e3603bc11aa5a5c4356e903b5e8bda72f44bcc1b
nicholasbcarr <carrn7@hawaii.edu>
2022-04-25 08:18:09 -1000

    more tries

--- app/imports/ui/pages/FilterEvents.jsx
+++ app/imports/ui/pages/FilterEvents.jsx
@@ -5,6 +5,7 @@ import { Container, Loader, Card, Segment } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { AutoForm, SubmitField } from 'uniforms-semantic';
+import { _ } from 'meteor/underscore';
 import MultiSelectField from '../components/form-fields/MultiSelectField';
 import { Events } from '../../api/event/EventCollection';
 import { Interests } from '../../api/interest/InterestCollection';
@@ -12,35 +13,26 @@ import { EventInterest } from '../../api/interest/EventInterestCollection';
 import EventCard from '../components/EventCard';
 
 /** Create a schema to specify the structure of the data to appear in the form. */
-const makeSchema = (allEvents) => new SimpleSchema({
-  events: { type: Array, label: 'Events', optional: true },
-  'events.$': { type: String, allowedValues: allEvents },
+const makeSchema = (allInterests) => new SimpleSchema({
+  interests: { type: Array, label: 'Interests', optional: true },
+  'interests.$': { type: String, allowedValues: allInterests },
 });
 
 /** Renders the Profile Collection as a set of Cards. */
-const FilterEvents = ({ ready, events, interests, eventInterests }) => {
-  const [eventsArray, setEventsArray] = useState([]);
-  const [bridge, setBridge] = useState();
-  useEffect(() => {
-    setEventsArray(events);
-    const formSchema = makeSchema(interests.map(interest => interest._id));
-    // console.log(interests.map(interest => interest._id));
-    const tempBridge = new SimpleSchema2Bridge(formSchema);
-    setBridge(tempBridge);
-  }, [events, interests]);
+const FilterEvents = ({ ready }) => {
 
-  const submit = (data) => {
-    // this.setState({ events: data.events || [] });
-    console.log(data);
-  };
-  // const eventsByInterest = _.pluck(EventInterest.find({}, {}).fetch(), '_id');
-  // console.log(eventsByInterest);
-  // const eventIDArray = eventsByInterest.map(eventByInterest => eventByInterest.eventID);
-  // console.log(eventIDArray);
-  // const events = eventIDArray.map(eventID => _.unique(Events.findOne({ eventID: eventID }, {})));
-  // console.log(events);
-  // const formSchema = makeSchema(events);
-  // const bridge = new SimpleSchema2Bridge(formSchema);
+  const allInterests = _.pluck(Interests.find({}, {}).fetch(), 'name');
+  //pull each interest from the collection to populate form
+  const formSchema = makeSchema(allInterests);
+  //make the schema into an array
+  const bridge = new SimpleSchema2Bridge(formSchema);
+  //bridge for the form options
+  //need build an array using state to collect selected interest to find associated events
+  const eventInterests = EventInterest.find({ category: { $in: this.state.interests } }, {}).fetch();
+  //find the events with the similar interest
+  const eventIDList = eventInterests.map(eventInterest => eventInterest.eventID);
+  //get the list of event id's
+  console.log(eventInterests);
 
   if (!ready) {
     return <Loader active>Getting data</Loader>;
@@ -51,11 +43,11 @@ const FilterEvents = ({ ready, events, interests, eventInterests }) => {
     <Container id="filter-page">
       <AutoForm schema={bridge} onSubmit={data => submit(data)} >
         <Segment>
+          <MultiSelectField id='interests' name='interests' showInlineError={true} placeholder={'Interests'}/>
           <SubmitField id='submit' value='Submit'/>
         </Segment>
       </AutoForm>
       <Card.Group style={{ paddingTop: '10px' }}>
-        {eventsArray.map((event, index) => (<EventCard key={index} event={event}/>))}
       </Card.Group>
     </Container>
   );
@@ -64,9 +56,6 @@ const FilterEvents = ({ ready, events, interests, eventInterests }) => {
 /** Require an array of Stuff documents in the props. */
 FilterEvents.propTypes = {
   ready: PropTypes.bool.isRequired,
-  events: PropTypes.array.isRequired,
-  interests: PropTypes.array.isRequired,
-  eventInterests: PropTypes.array.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
@@ -75,13 +64,7 @@ export default withTracker(() => {
   const subscription2 = Interests.subscribe();
   const subscription3 = EventInterest.subscribe();
   const ready = subscription1.ready() && subscription2.ready() && subscription3.ready();
-  const events = Events.find({}, { sort: { name: 1 } }).fetch();
-  const interests = Interests.find({}, {}).fetch();
-  const eventInterests = EventInterest.find({}, {}).fetch();
   return {
-    events,
-    interests,
-    eventInterests,
     ready,
   };
 })(FilterEvents);
