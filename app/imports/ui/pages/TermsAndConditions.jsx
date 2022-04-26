fd18760d86a6f9551870ed56097a21b812bdb583
Tracy Bui <89670935+tbui00@users.noreply.github.com>
2022-04-25 18:58:48 -1000

    Updated TermsAndConditions.jsx

--- app/imports/ui/pages/TermsAndConditions.jsx
+++ app/imports/ui/pages/TermsAndConditions.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Header } from 'semantic-ui-react';
+import { Grid, Header, Item, Divider } from 'semantic-ui-react';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
 /** Renders the Page for adding a document. */
@@ -12,25 +12,38 @@ const TermsAndConditions = () => (
       <div className="row, about-us-section">
         <Header as="h2" textAlign="left align"> Terms of Use </Header>
         <p>
-            Last updated July 01, 2020 <br/>
-
-            AGREEMENT TO TERMS <br/>
-
-          {/* eslint-disable-next-line max-len */}
-            These Terms of Use constitute a legally binding agreement made between you, whether personally or on behalf of an entity (“you”) and Volunteer Ally, LLC (“Company“, “we”, “us”, or “our”), concerning your access to and use of the https://volunteerally.org website as well as any other media form, media channel, mobile website or mobile application related, linked, or otherwise connected thereto (collectively, the “Site”). You agree that by accessing the Site, you have read, understood, and agreed to be bound by all of these Terms of Use. IF YOU DO NOT AGREE WITH ALL OF THESE TERMS OF USE, THEN YOU ARE EXPRESSLY PROHIBITED FROM USING THE SITE AND YOU MUST DISCONTINUE USE IMMEDIATELY. <br/>
-
-          {/* eslint-disable-next-line max-len */}
-            Supplemental terms and conditions or documents that may be posted on the Site from time to time are hereby expressly incorporated herein by reference. We reserve the right, in our sole discretion, to make changes or modifications to these Terms of Use at any time and for any reason. We will alert you about any changes by updating the “Last updated” date of these Terms of Use, and you waive any right to receive specific notice of each such change. It is your responsibility to periodically review these Terms of Use to stay informed of updates. You will be subject to, and will be deemed to have been made aware of and to have accepted, the changes in any revised Terms of Use by your continued use of the Site after the date such revised Terms of Use are posted. <br/>
-
-          {/* eslint-disable-next-line max-len */}
-            The information provided on the Site is not intended for distribution to or use by any person or entity in any jurisdiction or country where such distribution or use would be contrary to law or regulation or which would subject us to any registration requirement within such jurisdiction or country. Accordingly, those persons who choose to access the Site from other locations do so on their own initiative and are solely responsible for compliance with local laws, if and to the extent local laws are applicable. <br/>
-
-          {/* eslint-disable-next-line max-len */}
-            The Site is not tailored to comply with industry-specific regulations (Health Insurance Portability and Accountability Act (HIPAA), Federal Information Security Management Act (FISMA), etc.), so if your interactions would be subjected to such laws, you may not use this Site. You may not use the Site in a way that would violate the Gramm-Leach-Bliley Act (GLBA). <br/>
-
-          {/* eslint-disable-next-line max-len */}
-            The Site is intended for users who are at least 13 years of age. All users who are minors in the jurisdiction in which they reside (generally under the age of 18) must have the permission of, and be directly supervised by, their parent or guardian to use the Site. If you are a minor, you must have your parent or guardian read and agree to these Terms of Use prior to you using the Site. <br/>
-
+          <Item.Group>
+            <Item.Content>
+              <Item.Meta>Last updated July 01, 2020</Item.Meta>
+              <Divider hidden />
+              <Header as="h3" textAlign="left align">AGREEMENT TO TERMS</Header>
+              <Item.Meta>
+                {/* eslint-disable-next-line max-len */}
+                These Terms of Use constitute a legally binding agreement made between you, whether personally or on behalf of an entity (“you”) and Volunteer Ally, LLC (“Company“, “we”, “us”, or “our”), concerning your access to and use of the https://volunteerally.org website as well as any other media form, media channel, mobile website or mobile application related, linked, or otherwise connected thereto (collectively, the “Site”). You agree that by accessing the Site, you have read, understood, and agreed to be bound by all of these Terms of Use. IF YOU DO NOT AGREE WITH ALL OF THESE TERMS OF USE, THEN YOU ARE EXPRESSLY PROHIBITED FROM USING THE SITE AND YOU MUST DISCONTINUE USE IMMEDIATELY.
+              </Item.Meta>
+              <Divider hidden />
+              <Item.Meta>
+                {/* eslint-disable-next-line max-len */}
+                Supplemental terms and conditions or documents that may be posted on the Site from time to time are hereby expressly incorporated herein by reference. We reserve the right, in our sole discretion, to make changes or modifications to these Terms of Use at any time and for any reason. We will alert you about any changes by updating the “Last updated” date of these Terms of Use, and you waive any right to receive specific notice of each such change. It is your responsibility to periodically review these Terms of Use to stay informed of updates. You will be subject to, and will be deemed to have been made aware of and to have accepted, the changes in any revised Terms of Use by your continued use of the Site after the date such revised Terms of Use are posted. <br/>
+              </Item.Meta>
+              <Divider hidden />
+              <Item.Meta>
+                {/* eslint-disable-next-line max-len */}
+                The information provided on the Site is not intended for distribution to or use by any person or entity in any jurisdiction or country where such distribution or use would be contrary to law or regulation or which would subject us to any registration requirement within such jurisdiction or country. Accordingly, those persons who choose to access the Site from other locations do so on their own initiative and are solely responsible for compliance with local laws, if and to the extent local laws are applicable. <br/>
+              </Item.Meta>
+              <Divider hidden />
+              <Item.Meta>
+                {/* eslint-disable-next-line max-len */}
+                The Site is not tailored to comply with industry-specific regulations (Health Insurance Portability and Accountability Act (HIPAA), Federal Information Security Management Act (FISMA), etc.), so if your interactions would be subjected to such laws, you may not use this Site. You may not use the Site in a way that would violate the Gramm-Leach-Bliley Act (GLBA). <br/>
+              </Item.Meta>
+              <Divider hidden />
+              <Item.Meta>
+                {/* eslint-disable-next-line max-len */}
+                The Site is intended for users who are at least 13 years of age. All users who are minors in the jurisdiction in which they reside (generally under the age of 18) must have the permission of, and be directly supervised by, their parent or guardian to use the Site. If you are a minor, you must have your parent or guardian read and agree to these Terms of Use prior to you using the Site. <br/>
+              </Item.Meta>
+              <Divider hidden />
+            </Item.Content>
+          </Item.Group>
             INTELLECTUAL PROPERTY RIGHTS <br/>
 
           {/* eslint-disable-next-line max-len */}
