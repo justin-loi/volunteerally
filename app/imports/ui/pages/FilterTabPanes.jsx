99bbde844ee1793764370b4727fb2922cabda08a
nicholasbcarr <carrn7@hawaii.edu>
2022-04-27 14:24:08 -1000

    made react-name for filterpanes

--- app/imports/ui/pages/FilterTabPanes.jsx
+++ app/imports/ui/pages/FilterTabPanes.jsx
@@ -6,33 +6,33 @@ import FilterEnvironments from '../components/filters/FilterEnvironments';
 import FilterSkills from '../components/filters/FilterSkills';
 
 /* Renders a list of events. Use <EventCard> to render each event card. */
-const FilterTabPanes = () => (
-  <Tab menu={{ fluid: true, vertical: true, tabular: true }} panes={
+export default function FilterTabPanes() {
+  return (
+    <Tab menu={{ fluid: true, vertical: true, tabular: true }} panes={
     // eslint-disable-next-line react/display-name
-    [{ menuItem: 'Opportunities By Organization', render: () => <Tab.Pane>
-      <Container>
-        <FilterOrganizations/>
-      </Container>
-    </Tab.Pane> },
-    // eslint-disable-next-line react/display-name
-    { menuItem: 'Opportunities By Interest', render: () => <Tab.Pane>
-      <Container>
-        <FilterInterests/>
-      </Container>
-    </Tab.Pane> },
-    // eslint-disable-next-line react/display-name
-    { menuItem: 'Opportunities By Environment', render: () => <Tab.Pane>
-      <Container>
-        <FilterEnvironments/>
-      </Container>
-    </Tab.Pane> },
-    { menuItem: 'Opportunities By Special Skills', render: () => <Tab.Pane>
-      <Container>
-        <FilterSkills/>
-      </Container>
-    </Tab.Pane> },
-    ]
-  } />
-);
-
-export default FilterTabPanes;
+      [{ menuItem: 'Opportunities By Organization', render: () => <Tab.Pane>
+        <Container>
+          <FilterOrganizations/>
+        </Container>
+      </Tab.Pane> },
+      // eslint-disable-next-line react/display-name
+      { menuItem: 'Opportunities By Interest', render: () => <Tab.Pane>
+        <Container>
+          <FilterInterests/>
+        </Container>
+      </Tab.Pane> },
+      // eslint-disable-next-line react/display-name
+      { menuItem: 'Opportunities By Environment', render: () => <Tab.Pane>
+        <Container>
+          <FilterEnvironments/>
+        </Container>
+      </Tab.Pane> },
+      { menuItem: 'Opportunities By Special Skills', render: () => <Tab.Pane>
+        <Container>
+          <FilterSkills/>
+        </Container>
+      </Tab.Pane> },
+      ]
+    } />
+  );
+}
