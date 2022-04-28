3c67d4d32321043a566fe679ac053568dce80d7a
nicholasbcarr <carrn7@hawaii.edu>
2022-04-27 14:44:09 -1000

    updated display name
    filterpanes

--- app/imports/ui/pages/FilterTabPanes.jsx
+++ app/imports/ui/pages/FilterTabPanes.jsx
@@ -6,33 +6,43 @@ import FilterEnvironments from '../components/filters/FilterEnvironments';
 import FilterSkills from '../components/filters/FilterSkills';
 
 /* Renders a list of events. Use <EventCard> to render each event card. */
-export default function FilterTabPanes() {
-  return (
-    <Tab menu={{ fluid: true, vertical: true, tabular: true }} panes={
-    // eslint-disable-next-line react/display-name
-      [{ menuItem: 'Opportunities By Organization', render: () => <Tab.Pane>
+const FilterTabPanes = () => (
+  <Tab menu={{ fluid: true, vertical: true, tabular: true }} panes={
+    [{
+      // eslint-disable-next-line react/display-name
+      menuItem: 'Opportunities By Organization', render: () => <Tab.Pane>
         <Container>
           <FilterOrganizations/>
         </Container>
-      </Tab.Pane> },
+      </Tab.Pane>,
+    },
+    {
       // eslint-disable-next-line react/display-name
-      { menuItem: 'Opportunities By Interest', render: () => <Tab.Pane>
+      menuItem: 'Opportunities By Interest', render: () => <Tab.Pane>
         <Container>
           <FilterInterests/>
         </Container>
-      </Tab.Pane> },
+      </Tab.Pane>,
+    },
+
+    {
       // eslint-disable-next-line react/display-name
-      { menuItem: 'Opportunities By Environment', render: () => <Tab.Pane>
+      menuItem: 'Opportunities By Environment', render: () => <Tab.Pane>
         <Container>
           <FilterEnvironments/>
         </Container>
-      </Tab.Pane> },
-      { menuItem: 'Opportunities By Special Skills', render: () => <Tab.Pane>
+      </Tab.Pane>,
+    },
+    {
+      // eslint-disable-next-line react/display-name
+      menuItem: 'Opportunities By Special Skills', render: () => <Tab.Pane>
         <Container>
           <FilterSkills/>
         </Container>
-      </Tab.Pane> },
-      ]
-    } />
-  );
-}
+      </Tab.Pane>,
+    },
+    ]
+  }/>
+);
+
+export default FilterTabPanes;
